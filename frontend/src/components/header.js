0be3a0cc071db95bfffa0bedc49b2292f8ff4cd4
nagesh <nageshbabu921@gmail.com>
2021-09-11 12:03:31 +0530

    frontend

---
+++ frontend/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div>
+        <h1 style={{
+          color:'blue'
+  
+        }}>Drop your problem</h1>
+      </div>
+    )
+}
+
+export default header
